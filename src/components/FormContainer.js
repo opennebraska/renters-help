4162438753c8bbab35bd5d6a92a4dc480faff44e
erikdodge <edodge-chwy>
2020-11-11 23:49:36 -0600

    refactor step functions

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Start from "./steps/qualification/Start";
 import {Button} from "@material-ui/core";
 import IncomeQualifications from "./steps/qualification/IncomeQualifications";
@@ -9,83 +9,56 @@ import UnsafeLivingConditions from "./steps/qualification/UnsafeLivingConditions
 import AttemptingToPay from "./steps/qualification/AttemptingToPay";
 import Qualified from "./steps/qualification/Qualified";
 import PersonalInformation from "./steps/letter-builder/PersonalInformation";
-import {LetterBuilderSteps, QualificationSteps} from "./steps/StepNames";
 import LandlordInfo from "./steps/letter-builder/LandlordInfo";
 import PreviewLetter from "./steps/letter-builder/PreviewLetter";
 import Signature from "./steps/letter-builder/Signature";
 import DownloadLetter from "./steps/letter-builder/DownloadLetter";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import {nextStep, previousStep} from "./steps/StepFunctions";
 
-export default class FormContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentStep: 0,
-            formNumber: 1,
-        }
-    }
+export default function FormContainer() {
+    const [state, setState] = useState({
+        currentStep: 0,
+        formNumber: 1,
+    });
 
-    render() {
-        const {currentStep, formNumber} = this.state;
-        return (
-            <React.Fragment>
-                <Grid container dirction='column' alignItems='center' justify='center'>
-                    <Grid item xs={12} md={4}>
-                        <Paper  style={{padding: '15px'}}>
-                            <h1>Renters Help form</h1>
-                            <StepCounter formNumber={formNumber} currentStep={currentStep}/>
+    const {currentStep, formNumber} = state;
+    return (
+        <React.Fragment>
+            <Grid container dirction='column' alignItems='center' justify='center'>
+                <Grid item xs={12} md={4}>
+                    <Paper style={{padding: '15px'}}>
+                        <h1>Renters Help form</h1>
+                        <StepCounter formNumber={formNumber} currentStep={currentStep}/>
 
-                            {formNumber === 1 &&
-                            <form onSubmit={() => alert('Submited')}>
-                                <Start currentStep={currentStep}/>
-                                <IncomeQualifications currentStep={currentStep}/>
-                                <GovernmentHelp currentStep={currentStep}/>
-                                <AffordRent currentStep={currentStep}/>
-                                <UnsafeLivingConditions currentStep={currentStep}/>
-                                <AttemptingToPay currentStep={currentStep}/>
-                                <Qualified currentStep={currentStep}/>
-                            </form>
-                            }
+                        {formNumber === 1 &&
+                        <form onSubmit={() => alert('Submited')}>
+                            <Start currentStep={currentStep}/>
+                            <IncomeQualifications currentStep={currentStep}/>
+                            <GovernmentHelp currentStep={currentStep}/>
+                            <AffordRent currentStep={currentStep}/>
+                            <UnsafeLivingConditions currentStep={currentStep}/>
+                            <AttemptingToPay currentStep={currentStep}/>
+                            <Qualified currentStep={currentStep}/>
+                        </form>
+                        }
 
-                            {formNumber === 2 &&
-                            <form onSubmit={() => alert('Submited')}>
-                                <PersonalInformation currentStep={currentStep}/>
-                                <LandlordInfo currentStep={currentStep}/>
-                                <PreviewLetter currentStep={currentStep}/>
-                                <Signature currentStep={currentStep}/>
-                                <DownloadLetter currentStep={currentStep}/>
-                            </form>
-                            }
+                        {formNumber === 2 &&
+                        <form onSubmit={() => alert('Submited')}>
+                            <PersonalInformation currentStep={currentStep}/>
+                            <LandlordInfo currentStep={currentStep}/>
+                            <PreviewLetter currentStep={currentStep}/>
+                            <Signature currentStep={currentStep}/>
+                            <DownloadLetter currentStep={currentStep}/>
+                        </form>
+                        }
 
-                            <Button variant='contained' onClick={this.previousStep}>Previous</Button>
-                            <Button variant='contained' color='primary' onClick={this.nextStep}>Next</Button>
-                        </Paper>
-                    </Grid>
+                        <Button variant='contained' onClick={() => previousStep(state, setState)}>Previous</Button>
+                        <Button variant='contained' color='primary' onClick={() => nextStep(state, setState)}>Next</Button>
+                    </Paper>
                 </Grid>
-            </React.Fragment>
-        )
-    }
-
-    nextStep = () => {
-        let {currentStep, formNumber} = this.state;
-        if (formNumber === 1 && currentStep === QualificationSteps.FINAL_STEP) {
-            formNumber = 2;
-            currentStep = 1;
-        } else {
-            currentStep = this.state.currentStep + 1;
-        }
-        this.setState({currentStep, formNumber});
-    }
-
-    previousStep = () => {
-        let {currentStep, formNumber} = this.state;
-        if (formNumber === 2 && currentStep === LetterBuilderSteps.PERSONAL_INFO) {
-            formNumber = 1;
-            currentStep = QualificationSteps.FINAL_STEP;
-        } else if (!(formNumber === 1 && currentStep === QualificationSteps.START)) {
-            currentStep = currentStep - 1;
-        }
-        this.setState({currentStep, formNumber})
-    }
+            </Grid>
+        </React.Fragment>
+    )
 }
---
+++ src/components/steps/StepFunctions.js
@@ -0,0 +1,25 @@
+import {LetterBuilderSteps, QualificationSteps} from "./StepNames";
+
+export const nextStep = (state, setState, validate = () => true) => {
+    if (validate()) {
+        let {currentStep, formNumber} = state;
+        if (formNumber === 1 && currentStep === QualificationSteps.FINAL_STEP) {
+            formNumber = 2;
+            currentStep = 1;
+        } else {
+            currentStep = state.currentStep + 1;
+        }
+        setState({currentStep, formNumber});
+    }
+}
+
+export const previousStep = (state, setState) => {
+    let {currentStep, formNumber} = state;
+    if (formNumber === 2 && currentStep === LetterBuilderSteps.PERSONAL_INFO) {
+        formNumber = 1;
+        currentStep = QualificationSteps.FINAL_STEP;
+    } else if (!(formNumber === 1 && currentStep === QualificationSteps.START)) {
+        currentStep = currentStep - 1;
+    }
+    setState({currentStep, formNumber})
+}
