3df28b6ffb6099cf44dcd6bca7cebdaf385019f0
erikdodge <edodge-chwy>
2020-11-12 22:59:13 -0600

    Validation on Qualification step 5. Fixes #7

--- src/components/steps/qualification/AttemptingToPay.js
+++ src/components/steps/qualification/AttemptingToPay.js
@@ -4,11 +4,11 @@ import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/styles";
 import {QualificationSteps} from "../StepNames";
 import FormControl from "@material-ui/core/FormControl";
-import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Button from "@material-ui/core/Button";
 import {nextStep, previousStep} from "../StepFunctions";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
 
 const useStyles = makeStyles(() => ({
     root: {
@@ -23,25 +23,23 @@ const useStyles = makeStyles(() => ({
     },
 }));
 
+const validate = (selected, state, setState) => {
+    if(selected === 'yes') {
+        return true
+    } else {
+        const errorStep = state.currentStep * -1;
+        setState({...state, currentStep: errorStep});
+    }
+}
+
 export default function AttemptingToPay({state, setState}) {
 
     const classes = useStyles();
-    const [form, setForm] = React.useState({
-        yes: false,
-        no: false,
-        lostSubstantialIncome: false,
-        lostJob: false,
-        hoursCut: false,
-        salaryReduced: false,
-        extraordinaryMedicalCost: false,
-        none: false
-    })
+    const [selected, setSelected] = React.useState('none')
 
     const handleChange = (event) => {
-        setForm({...form, [event.target.name]: event.target.checked})
+        setSelected(event.target.value)
     }
-
-    const {yes, no} = form;
     if (state.currentStep !== QualificationSteps.ATTEMPTING_TO_PAY) {
         return null
     }
@@ -51,22 +49,15 @@ export default function AttemptingToPay({state, setState}) {
             I am doing my best to pay as much rent as I can, given my circumstances
         </Typography>
         <FormControl component="fieldset" className={classes.formControl}>
-            <FormGroup>
-                <FormControlLabel
-                    control={<Checkbox checked={yes} onChange={handleChange} name="yes"/>}
-                    label="Yes"
-                />
-                <FormControlLabel
-                    control={<Checkbox checked={no} onChange={handleChange}
-                                       name="no"/>}
-                    label="No"
-                />
-            </FormGroup>
+            <RadioGroup value={selected} onChange={handleChange}>
+                <FormControlLabel value="yes" control={<Radio />} label="yes" />
+                <FormControlLabel value="no" control={<Radio />} label="no" />
+            </RadioGroup>
 
         </FormControl>
         <div>
-            <Button variant='contained' onClick={() => previousStep(state, setState)}>Previous</Button>
-            <Button variant='contained' color='primary' onClick={() => nextStep(state, setState)}>Next</Button>
+            <Button variant='contained' style={{marginRight: '20px'}} onClick={() => previousStep(state, setState)}>Previous</Button>
+            <Button variant='contained' color='primary' disabled={'none' === selected} onClick={() => nextStep(state, setState, () => validate(selected, state, setState))}>Next</Button>
         </div>
     </React.Fragment>)
 }
