5c7dd8e6b71903545442d90751c6e3c259605640
Ron Watts <Ron.Watts@proxibid.com>
2020-11-22 18:17:16 -0600

    Make the toolbar icon + text a link to the home page

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -18,8 +18,10 @@ import Container from '@material-ui/core/Container';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 import {createMuiTheme} from "@material-ui/core";
 import {ThemeProvider} from "@material-ui/styles";
+import FlexContainer from "./FlexContainer";
 
 export default function FormContainer() {
   const [state, setState] = useState({
@@ -36,53 +38,55 @@ export default function FormContainer() {
     zip: '',
   });
   const [landlordInfo, setLandlordInfo] = useState({landlordCompany: '', landlordFullName: ''});
-const theme = createMuiTheme({palette: {primary: {main: '#DC2A44'}}})
+  const theme = createMuiTheme({palette: {primary: {main: '#DC2A44'}}})
   const {currentStep, formNumber} = state;
   return (
       <ThemeProvider theme={theme}>
         <AppBar position="fixed">
           <Toolbar>
-            <img src="/renters-help/neb-renter-help-favicon-red.png" alt="logo" style={{maxWidth: 30, marginRight: 10}} width={30} height={30}/>
-            <Typography variant="h6" noWrap>
-              Nebraska Renters Help
-            </Typography>
+            <Link component={"a"} href={process.env.PUBLIC_URL} style={{textDecoration: "none", color: "white", verticalAlign: "middle"}}>
+              <FlexContainer>
+                <img src={`${process.env.PUBLIC_URL}/neb-renter-help-favicon-red.png`} alt="logo" style={{marginRight: 10}} width={30} height={30}/>
+                <Typography variant="h6" component={"span"} noWrap>Nebraska Renters Help</Typography>
+              </FlexContainer>
+            </Link>
           </Toolbar>
         </AppBar>
         {formNumber === 0 ? <Home state={state} setState={setState}/> :
-        <Box mt={10}>
-          <Container maxWidth={'md'}>
-            <Paper elevation={3}>
-              <Box display={'flex'} flexDirection={'column'} p={1} m={3}>
-                <StepCounter formNumber={formNumber} currentStep={currentStep}/>
+            <Box mt={10}>
+              <Container maxWidth={'md'}>
+                <Paper elevation={3}>
+                  <Box display={'flex'} flexDirection={'column'} p={1} m={3}>
+                    <StepCounter formNumber={formNumber} currentStep={currentStep}/>
 
-                {formNumber === 1 &&
-                <form>
-                  <Start state={state} setState={setState}/>
-                  <IncomeQualifications state={state} setState={setState}/>
-                  <GovernmentHelp state={state} setState={setState}/>
-                  <AffordRent state={state} setState={setState}/>
-                  <UnsafeLivingConditions state={state} setState={setState}/>
-                  <AttemptingToPay state={state} setState={setState}/>
-                  <Qualified state={state} setState={setState}/>
-                </form>
-                }
+                    {formNumber === 1 &&
+                    <form>
+                      <Start state={state} setState={setState}/>
+                      <IncomeQualifications state={state} setState={setState}/>
+                      <GovernmentHelp state={state} setState={setState}/>
+                      <AffordRent state={state} setState={setState}/>
+                      <UnsafeLivingConditions state={state} setState={setState}/>
+                      <AttemptingToPay state={state} setState={setState}/>
+                      <Qualified state={state} setState={setState}/>
+                    </form>
+                    }
 
-                {formNumber === 2 &&
-                <form>
-                  <PersonalInformation state={state} setState={setState} renterInfo={renterInfo} setRenterInfo={setRenterInfo}/>
-                  <LandlordInfo state={state} setState={setState} landlordInfo={landlordInfo} setLandlordInfo={setLandlordInfo}/>
-                  <PreviewLetter state={state} setState={setState} renterInfo={renterInfo} landlordInfo={landlordInfo}/>
-                </form>
-                }
-                {currentStep < 0 &&
-                <div>
-                  <FailedQualification state={state} setState={setState}/>
-                </div>
-                }
-              </Box>
-            </Paper>
-          </Container>
-        </Box>
+                    {formNumber === 2 &&
+                    <form>
+                      <PersonalInformation state={state} setState={setState} renterInfo={renterInfo} setRenterInfo={setRenterInfo}/>
+                      <LandlordInfo state={state} setState={setState} landlordInfo={landlordInfo} setLandlordInfo={setLandlordInfo}/>
+                      <PreviewLetter state={state} setState={setState} renterInfo={renterInfo} landlordInfo={landlordInfo}/>
+                    </form>
+                    }
+                    {currentStep < 0 &&
+                    <div>
+                      <FailedQualification state={state} setState={setState}/>
+                    </div>
+                    }
+                  </Box>
+                </Paper>
+              </Container>
+            </Box>
         }
       </ThemeProvider>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {makeStyles} from "@material-ui/styles";
 import Box from '@material-ui/core/Box';
 import Button from "@material-ui/core/Button"
+import Link from "@material-ui/core/Link"
 import {createMuiTheme} from "@material-ui/core";
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import EmailIcon from '@material-ui/icons/Email';
@@ -10,95 +11,98 @@ import DescriptionIcon from '@material-ui/icons/Description';
 import * as ReactGA from "react-ga";
 
 const useStyles = makeStyles(() => ({
-    root: {
-        flexGrow: 1,
-        maxWidth: 752,
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  boxColor: {
+    backgroundColor: '#E9B5BD'
+  },
+  demo: {
+    backgroundColor: 'white',
+  },
+  bigText: {
+    [theme.breakpoints.down('lg')]: {
+      fontSize: '70px'
     },
-    boxColor: {
-        backgroundColor: '#E9B5BD'
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '34px'
     },
-    demo: {
-        backgroundColor: 'white',
+  },
+  block: {
+    [theme.breakpoints.up('lg')]: {
+      padding: '32px',
+      display: 'inline-block'
     },
-    bigText: {
-        [theme.breakpoints.down('lg')]: {
-            fontSize: '70px'
-        },
-        [theme.breakpoints.down('sm')]: {
-            fontSize: '34px'
-        },
-    },
-    block: {
-        [theme.breakpoints.up('lg')]: {
-            padding: '32px',
-            display: 'inline-block'
-        },
-        [theme.breakpoints.up('md')]: {
-            padding: '16px',
-            display: 'inline-block'
-        }
-    },
-    icon: {
-        fontSize: '80px',
-        color: '#DC2A44'
+    [theme.breakpoints.up('md')]: {
+      padding: '16px',
+      display: 'inline-block'
     }
+  },
+  icon: {
+    fontSize: '80px',
+    color: '#DC2A44'
+  }
 }));
 
 const theme = createMuiTheme({palette: {primary: {main: '#DC2A44'}}})
 
 export default function Home({state, setState}) {
-    const classes = useStyles();
-    useEffect(() => ReactGA.pageview('home'), []);
-    return (
-        <React.Fragment>
-            <div style={{textAlign: "center"}}>
-                <Box pt={4} pb={6} pl={[2, 4, 6, 24]} pr={[2, 4, 6, 24]} className={classes.boxColor}>
-                    <h1 className={classes.bigText}>Are you facing an <span style={{color: '#DC2A44'}}>eviction</span>? This tool can help.</h1>
-                    <p>If you're having trouble keeping up with your rent payments, the Federal Government has issued an order that prevents
-                        landlords in the US from evicting many of their tenants until after December 31, 2020. Use this tool to see if you are covered,
-                        and to produce the right letter (called a "Declaration") to send to your landlord, as required by the order.</p>
-                    <Button style={{borderRadius: 15, textTransform:'none', height:50, width:200}} color='primary' variant='contained' disableElevation
-                    onClick={() => {setState({...state, formNumber: 1})}}>
-                        Start Free Tool
-                    </Button>
-                </Box>
-                <Box>
-                    <div className={classes.block}>
-                        <ListAltIcon className={classes.icon}/>
-                        <h3>Fill out the form in 5 minutes</h3>
-                        <p>Answer a few questions and provide your<br/>landlord's contact information</p>
-                    </div>
-                    <div className={classes.block}>
-                        <DescriptionIcon className={classes.icon}/>
-                        <h3>The tool will create a letter</h3>
-                        <p>The app will create a letter that is ready<br/>to send based on your answers</p>
-                    </div>
-                    <div className={classes.block}>
-                        <EmailIcon className={classes.icon}/>
-                        <h3>Send the letter to your landlord</h3>
-                        <p>Download or print the letter to<br/>send to your landlord</p>
-                    </div>
-                </Box>
-                <Box pt={2} pb={2} pl={[4, 8, 16, 40]} pr={[4, 8, 16, 40]}>
-                    <h2>
-                        About Nebraska Renters Help
-                    </h2>
-                    <p>
-                        Nebraska Renters Help was built by Code for Nebraska.
-                        The purpose of the site is to provide self-help tools and resources to struggling
-                        Nebraska renters so that they can obtain protection they are entitled to under the current
-                        CDC Federal Eviction Moratorium.
-                    </p>
-                </Box>
-                <Box pt={10}>
-                    <footer>
-                        <p>2020 <a href="https://codefornebraska.org/" target="_blank" rel="nofollow">Code for Nebraska</a></p>
-                    </footer>
-                </Box>
+  const classes = useStyles();
+  useEffect(() => ReactGA.pageview('home'), []);
+  return (
+      <>
+        <div style={{textAlign: "center"}}>
+          <Box pt={4} pb={6} pl={[2, 4, 6, 24]} pr={[2, 4, 6, 24]} className={classes.boxColor}>
+            <h1 className={classes.bigText}>Are you facing an <span style={{color: '#DC2A44'}}>eviction</span>? This tool can help.</h1>
+            <p>If you're having trouble keeping up with your rent payments, the Federal Government has issued an order that prevents
+              landlords in the US from evicting many of their tenants until after December 31, 2020. Use this tool to see if you are covered,
+              and to produce the right letter (called a "Declaration") to send to your landlord, as required by the order.</p>
+            <Button style={{borderRadius: 15, textTransform: 'none', height: 50, width: 200}} color='primary' variant='contained' disableElevation
+                    onClick={() => {
+                      setState({...state, formNumber: 1})
+                    }}>
+              Start Free Tool
+            </Button>
+          </Box>
+          <Box>
+            <div className={classes.block}>
+              <ListAltIcon className={classes.icon}/>
+              <h3>Fill out the form in 5 minutes</h3>
+              <p>Answer a few questions and provide your<br/>landlord's contact information</p>
             </div>
-        </React.Fragment>)
+            <div className={classes.block}>
+              <DescriptionIcon className={classes.icon}/>
+              <h3>The tool will create a letter</h3>
+              <p>The app will create a letter that is ready<br/>to send based on your answers</p>
+            </div>
+            <div className={classes.block}>
+              <EmailIcon className={classes.icon}/>
+              <h3>Send the letter to your landlord</h3>
+              <p>Download or print the letter to<br/>send to your landlord</p>
+            </div>
+          </Box>
+          <Box pt={2} pb={2} pl={[4, 8, 16, 40]} pr={[4, 8, 16, 40]}>
+            <h2>
+              About Nebraska Renters Help
+            </h2>
+            <p>
+              Nebraska Renters Help was built by Code for Nebraska.
+              The purpose of the site is to provide self-help tools and resources to struggling
+              Nebraska renters so that they can obtain protection they are entitled to under the current
+              CDC Federal Eviction Moratorium.
+            </p>
+          </Box>
+          <Box pt={10}>
+            <footer>
+              <p><Link color="primary" href="https://codefornebraska.org/" target="_blank" rel="nofollow">2020 Code for Nebraska</Link></p>
+            </footer>
+          </Box>
+        </div>
+      </>
+  );
 }
 
 Home.propTypes = {
-    formNumber: PropTypes.number,
+  formNumber: PropTypes.number,
 }
