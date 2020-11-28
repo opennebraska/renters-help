d5decd6957fa6d0766d7e8981a1bd62eb7b04e20
ed.snodgrass <snodgrass.ed@gmail.com>
2020-11-28 13:42:32 -0600

    #18 Putting the translations back into en.js that are used by the useTranslations hook

--- src/translations/en.js
+++ src/translations/en.js
@@ -1,6 +1,23 @@
 const translations =  {
   translation: {
 
+    homeAboutHeader: 'About Nebraska Renters Help',
+    homeAboutInfo: 'Nebraska Renters Help was built by Code for Nebraska. ' +
+        'The purpose of the site is to provide self-help tools and resources so struggling ' +
+        'Nebraska renters can obtain entitled protection under the current ' +
+        'CDC Federal Eviction Moratorium. The information in Nebraska Renters Help does not constitute legal ' +
+        'advice and must not be used as a substitute for the advice of a lawyer qualified to give advice ' +
+        'on legal issues pertaining to housing. We can help direct you to free legal services if ' +
+        'necessary.',
+    homeLanguagePreference: 'Prefiere Español?',
+    homeOtherRentalAssistance: 'Other Rental Assistance',
+    homeReasonToUse: "If you're having trouble keeping up with your rent payments, the Federal Government has issued an\n" +
+        "order that prevents\n" +
+        "landlords in the US from evicting many of their tenants until after December 31, 2020. Use this\n" +
+        "tool to see if you are covered,\n" +
+        "and to produce the right letter (called a \"Declaration\") to send to your landlord, as required\n" +
+        "by the order.",
+    homeStartButton: 'Start Free Tool',
   }
 }
 
