7d82e15a7d7c0aff22e674f3654e2761c60bdc51
ed.snodgrass <snodgrass.ed@gmail.com>
2020-11-28 12:19:52 -0600

    #18 Removing "TODO" text from es.js file

--- src/translations/es.js
+++ src/translations/es.js
@@ -1,8 +1,8 @@
 const translations = {
     translation: {
         homeLanguagePreference: 'Prefer English?',
-        homePageTitle: 'TODO Are you facing an <1>eviction</1>? This tool can help.',
-        homeReasonToUse: "TODO If you're having trouble keeping up with your rent payments, the Federal Government has issued an\n" +
+        homePageTitle: 'Are you facing an <1>eviction</1>? This tool can help.',
+        homeReasonToUse: "If you're having trouble keeping up with your rent payments, the Federal Government has issued an\n" +
             "order that prevents\n" +
             "landlords in the US from evicting many of their tenants until after December 31, 2020. Use this\n" +
             "tool to see if you are covered,\n" +
